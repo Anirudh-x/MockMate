cab8869312f8e3e9efb27eda775e4f4f9ace0867
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-02-18 23:32:12 +0530

    Authentication and Routing Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
-import { Button } from "@/components/ui/button"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { PublicLayout } from "@/layouts/PublicLayout";
+import Home from "@/routes/Home";
+import AuthLayout from "@/layouts/AuthLayout";
+import SignInPage from "./routes/SignInPage";
+import SignUpPage from "./routes/SignUpPage";
+import ProtectedLayouts from "./layouts/ProtectedLayouts";
+import { MainLayout } from "./layouts/MainLayout";
 
 const App = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
 
-export default App
+          {/* Public Routes */}
+          <Route element={<PublicLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* Authentication Layout */}
+          <Route element={<AuthLayout />}>
+            <Route path="/signin/*" element={<SignInPage />} />
+            <Route path="/signup/*" element={<SignUpPage />} />
+          </Route>
+
+          {/* Protected Routes */}
+          <Route path="/protected/*" element={<ProtectedLayouts><MainLayout/></ProtectedLayouts>} >
+          </Route>
+
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/routes/SignInPage.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@clerk/clerk-react"
+
+
+const SignInPage = () => {
+  return (
+    <div>
+      <SignIn path="/signin" />
+    </div>
+  )
+}
+
+export default SignInPage
