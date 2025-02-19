ac7fd57c3151e24435b8a1df0ba86ef366514e00
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-02-19 10:27:25 +0530

    Firebase configuration done

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import AuthHandlers from "@/handlers/AuthHandlers"
 import { Outlet } from "react-router-dom"
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
+      <AuthHandlers />
 
       <Header />
 
