971c6a7ce99bdd965f91dcfa338e9c3d740dcf8e
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-04-24 19:10:00 +0530

    Worked on Interview Generation

--- client-side/firebase/client.ts
+++ client-side/firebase/client.ts
@@ -4,14 +4,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBvasQw4ROk1ac053DeCibiBSYg3Dfu1FM",
+  authDomain: "mockmate-abcca.firebaseapp.com",
+  projectId: "mockmate-abcca",
+  storageBucket: "mockmate-abcca.firebasestorage.app",
+  messagingSenderId: "556358189042",
+  appId: "1:556358189042:web:3a4d3ef54ecabf8a30f56b",
+  measurementId: "G-HNC9ZG3TV2"
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
+// };
 
 // Initialize Firebase
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
